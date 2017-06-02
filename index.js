7c18bf88a3dc1bcf4f8eed18931b9eb2c6c4cb67
jamesford <jamie@stobbs.me>
2017-06-02 16:36:32 +0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const Discord = require('discord.js')
+const playMusic = require('./lib/play')
+const stopMusic = require('./lib/stop')
+const changeVolume = require('./lib/volume')
+
+// Setup
+const client = new Discord.Client()
+const bot_token = '<your bot token>'
+
+// Bot is running
+client.on('ready', () => {
+  console.log('Unit22 Online')
+})
+
+// Listen for messages
+client.on('message', message => {
+  if (message.content === '!ping') {
+    message.channel.send('pong :ping_pong:')
+  }
+
+  if (message.content === '!log') {
+    console.log(message)
+  }
+
+  if (message.content === '!where') {
+    const usersVoiceChannelID = message.member.voiceChannelID
+    
+    // Not in voice
+    if (!usersVoiceChannelID) return message.channel.send('Looks like you aren\'t in a voice channel :exclamation:')
+    
+    // Tell 'em
+    const usersVoiceChannel = message.member.guild.channels.get(usersVoiceChannelID)
+    return message.channel.send(`You're in ${usersVoiceChannel.name} on ${message.member.guild.name}`)
+  }
+
+  if (message.content === '!stop') {
+    stopMusic(message)
+  }
+
+  if (/^!play (\S+)$/i.test(message.content)) {
+    const url = message.content.match(/^!play (\S+)$/i)[1]
+    playMusic(message, url)
+  }
+
+  if (/^!volume (\d|10)$/i.test(message.content)) {
+    const volume = message.content.match(/^!volume (\d|10)$/i)[1] / 10
+    changeVolume(message, volume)
+  }
+})
+
+// Conenct/Login
+client.login(bot_token)
---
+++ notes.md
@@ -0,0 +1,2 @@
+Log the bot in
+  https://discordapp.com/oauth2/authorize?client_id=<CLIENT ID>&scope=bot
