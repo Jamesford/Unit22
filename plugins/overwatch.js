ccb883b9efe7669e323d747d0f5735b98d3790fd
jamesford <jamie@stobbs.me>
2017-06-15 17:04:05 +0200

    Working on overwatch plugin

--- plugins/overwatch.js
+++ plugins/overwatch.js
@@ -1,3 +1,4 @@
+const Discord = require('discord.js')
 const request = require('request')
 const _ = require('lodash')
 
@@ -17,51 +18,116 @@ function getStats (battletag) {
 }
 
 function statsFromMostPlayedRegion (stats) {
-  const regions = [stats.eu, stats.us, stats.kr].filter(v => v) // remove null values
-  if (regions.length < 1) return undefined // No playtime at all
-  if (regions.length === 1) return regions[0].stats
+  const regions = [
+    { region: 'eu', stats: stats.eu },
+    { region: 'us', stats: stats.us },
+    { region: 'kr', stats: stats.kr }
+  ].filter(r => r.stats)
 
-  let highest = { regionStats: undefined, time: 0 }
-  regions.forEach(region => {
-    let compTime = region.stats.competitive.game_stats.time_played
-    let quickTime = region.stats.quickplay.game_stats.time_played
+  if (regions.length < 1) return { error: 'No stats found for that battletag for any region' } // No playtime at all
+  if (regions.length === 1) return regions[0]
+
+  let highest = { time: 0, index: 0 }
+  regions.forEach((region, i) => {
+    let compTime = region.stats.stats.competitive.game_stats.time_played
+    let quickTime = region.stats.stats.quickplay.game_stats.time_played
     let totalTime = compTime + quickTime
     if (highest.time < totalTime) {
       highest.time = totalTime
-      highest.regionStats = region.stats
+      highest.index = i
     }
   })
 
-  return highest.regionStats
+  return regions[highest.index]
+}
+
+function getRegionStats (battletag, givenRegion, allStats) {
+  if (givenRegion && !allStats[givenRegion]) return { error: `No stats found for that battletag in the ${givenRegion} region` }
+  if (givenRegion && allStats[givenRegion]) {
+    return { region: givenRegion, stats: allStats[givenRegion].stats }
+  }
+  const { region, stats, error } = statsFromMostPlayedRegion(allStats)
+  return { region, stats: stats.stats, error}
+}
+
+function getRankIcon (skillRating) {
+  if (!skillRating) return null
+  if (skillRating < 1500) return `https://blzgdapipro-a.akamaihd.net/game/rank-icons/season-2/rank-1.png`
+  if (skillRating < 2000) return `https://blzgdapipro-a.akamaihd.net/game/rank-icons/season-2/rank-2.png`
+  if (skillRating < 2500) return `https://blzgdapipro-a.akamaihd.net/game/rank-icons/season-2/rank-3.png`
+  if (skillRating < 3000) return `https://blzgdapipro-a.akamaihd.net/game/rank-icons/season-2/rank-4.png`
+  if (skillRating < 3500) return `https://blzgdapipro-a.akamaihd.net/game/rank-icons/season-2/rank-5.png`
+  if (skillRating < 4000) return `https://blzgdapipro-a.akamaihd.net/game/rank-icons/season-2/rank-6.png`
+  if (skillRating >= 4000) return `https://blzgdapipro-a.akamaihd.net/game/rank-icons/season-2/rank-7.png`
+  return null
 }
 
 module.exports = (bot, config, client) => {
-  bot.command(/^!(ow|overwatch) ([^0-9\s](\S{2,11})#(\d{4}))( (eu|us|kr))?$/i, async (message, match) => {
+  bot.command(/^!(ow|overwatch) ([^0-9\s](\S{2,11})#(\d{4,}))( (eu|us|kr))?$/i, async (message, match) => {
     const battletag = match[2]
     const givenRegion = match[6]
 
-    let stats
+    let allStats
     try {
-      stats = await getStats(battletag)
+      allStats = await getStats(battletag)
     } catch (error) {
       console.error(error)
       return message.channel.send(`Couldn't get stats for that battletag`)
     }
 
-    if (givenRegion && !stats[givenRegion]) return message.channel.send(`No stats found for that battletag in the ${givenRegion} region`)
+    console.log(allStats)
 
-    let regionStats = givenRegion ? stats[givenRegion].stats : statsFromMostPlayedRegion(stats)
+    const { region, stats, error } = getRegionStats(battletag, givenRegion, allStats)
+    if (error) return message.channel.send(error)
 
-    if (!regionStats) return message.channel.send(`No stats found for that battletag`)
+    console.log(region, stats)
 
-    console.log(`regionStats`, regionStats)
+    const c = stats.quickplay
 
-    return message.channel.send(`Found`)
+    console.log(c)
+
+    let embed = new Discord.RichEmbed()
+
+    embed.setAuthor(battletag, getRankIcon(c.overall_stats.comprank) || c.overall_stats.avatar, `https://playoverwatch.com/en-gb/career/pc/${region}/${battletag.replace('#', '-')}`)
+
+    embed.setDescription(`${c.overall_stats.games} competitive games played (${c.overall_stats.win_rate}% won) over ${c.game_stats.time_played} hours.`)
+
+    embed.addField('Level', c.overall_stats.level + (c.overall_stats.prestige * 100), true)
+
+    embed.addField('Skill Rating', c.overall_stats.comprank || 'Unranked', true)
+
+    embed.addField('Most Played Hero', 'Unknown', true)
+
+    embed.addField('Eliminations', `**Average**: ${c.average_stats.eliminations_avg} (${(c.average_stats.eliminations_avg / c.average_stats.deaths_avg).toFixed(2)} KD)\n**Most**: ${c.game_stats.eliminations_most_in_game}\n**Total**: ${c.game_stats.eliminations}`, true)
+
+    embed.addField('Damage', `**Average**: ${c.average_stats.damage_done_avg}\n**Most**: ${c.game_stats.damage_done_most_in_game}\n**Total**: ${c.game_stats.damage_done}`, true)
+
+    embed.addField('Healing', `**Average**: ${c.average_stats.healing_done_avg}\n**Most**: ${c.game_stats.healing_done_most_in_game}\n**Total**: ${c.game_stats.healing_done}`, true)
+
+    embed.addField('Solo Kills', `**Average**: ${c.average_stats.solo_kills_avg}\n**Most**: ${c.game_stats.solo_kills_most_in_game}\n**Total**: ${c.game_stats.solo_kills}`, true)
+
+    embed.addField('Final Blows', `**Average**: ${c.average_stats.final_blows_avg}\n**Most**: ${c.game_stats.final_blows_most_in_game}\n**Total**: ${c.game_stats.final_blows}`, true)
+
+    embed.addField('Environmental', `**Kills**: ${c.game_stats.environmental_kills}\n**Deaths**: ${c.game_stats.environmental_deaths}`, true)
+
+    embed.addField('Objective Kills', `**Average**: ${c.average_stats.objective_kills_avg}\n**Most**: ${c.game_stats.objective_kills_most_in_game}\n**Total**: ${c.game_stats.objective_kills}`, true)
+
+    embed.addField('Offensive Assits', `**Average**: ${c.average_stats.offensive_assists_avg}\n**Most**: ${c.game_stats.offensive_assists_most_in_game}\n**Total**: ${c.game_stats.offensive_assists}`, true)
+
+    embed.addField('Defensive Assists', `**Average**: ${c.average_stats.defensive_assists_avg}\n**Most**: ${c.game_stats.defensive_assists_most_in_game}\n**Total**: ${c.game_stats.defensive_assists}`, true)
+
+    embed.addField('Objective Time', `**Average**: ${c.average_stats.objective_time_avg}\n**Most**: ${c.game_stats.objective_time_most_in_game}\n**Total**: ${c.game_stats.objective_time}`, true)
+
+    embed.addField('Time on Fire', `**Average**: ${c.average_stats.time_spent_on_fire_avg}\n**Most**: ${c.game_stats.time_spent_on_fire_most_in_game}\n**Total**: ${c.game_stats.time_spent_on_fire}`, true)
+
+    embed.setFooter(client.user.username, client.user.avatarURL)
+
+    return message.channel.send('', {embed})
   })
   .usage(`!ow <battletag> [region (us, eu, kr)]`)
   .define(`displays competitive statistics for the given battletag`)
 
-  bot.command(/^!(ow|overwatch) qp ([^0-9\s](\S{2,11})#(\d{4}))( (eu|us|kr))?$/i, async (message, match) => {
+  bot.command(/^!(ow|overwatch) qp ([^0-9\s](\S{2,11})#(\d{4,}))( (eu|us|kr))?$/i, async (message, match) => {
     return message.channel.send(`WIP`)
   })
   .usage(`!ow qp <battletag> [region (us, eu, kr)]`)
