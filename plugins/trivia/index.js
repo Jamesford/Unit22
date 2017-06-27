0e2ef044d614182fef3064d44f1e6590e57abeeb
jamesford <jamie@stobbs.me>
2017-06-27 15:46:12 +0200

    Tweak plugins file structure, add trivia plugin prototype

---
+++ plugins/trivia/index.js
@@ -0,0 +1,91 @@
+// Fisher-Yates Shuffle (from https://stackoverflow.com/a/6274398)
+function shuffle(array) {
+    let counter = array.length;
+
+    // While there are elements in the array
+    while (counter > 0) {
+        // Pick a random index
+        let index = Math.floor(Math.random() * counter);
+
+        // Decrease counter by 1
+        counter--;
+
+        // And swap the last element with it
+        let temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+
+    return array;
+}
+
+const default_trivia = require('./question_sets/default.js')
+
+const startGame = function (channelId, questionSet, pointsToWin) {
+  return {
+    id: channelId,
+    currentQuestion: null,
+    pointsToWin: pointsToWin,
+    points: {},
+    questionPool: [...shuffle(questionSet)]
+  }
+}
+
+let games = {}
+
+module.exports = (bot, config, client) => {
+  bot.command(/^!trivia$/i, (message, match) => {
+    if (games[message.channel.id]) return message.channel.send('Trivia game already in progress')
+
+    games[message.channel.id] = startGame(message.channel.id, default_trivia, 2)
+    
+    let game = games[message.channel.id]
+    game.currentQuestion = game.questionPool.pop()
+
+    message.channel.send('Starting a new trivia game! Default question set!')
+    setTimeout(() => {
+      message.channel.send(`\`\`\`${game.currentQuestion.question}\`\`\``)
+    }, 1000)
+  })
+  .usage('!trivia')
+  .define('starts a trivia game')
+
+  bot.command(/^!(ans|answer) (.*)$/i, (message, match) => {
+    if (!games[message.channel.id]) return message.channel.send('No trivia game is in progress')
+    if (!games[message.channel.id].currentQuestion) return message.channel.send(`Too slow ${message.author.username}!`)
+
+    let game = games[message.channel.id]
+    const attempt = match[2]
+    const answer = game.currentQuestion.answer
+
+    if (attempt.toLowerCase() !== answer.toLowerCase()) return message.channel.send(`*__${attempt}__* is not correct ${message.author.username}!`)
+
+    game.currentQuestion = null
+    game.points[message.author.username] ? game.points[message.author.username]++ : game.points[message.author.username] = 1
+    message.channel.send(`*__${attempt}__* is correct, 1 point for ${message.author.username}!`)
+
+    // No questions remaining, highest score wins
+    if (game.questionPool.length === 0) {
+      return setTimeout(() => {
+        message.channel.send(`${message.author.username} wins`)
+        delete game[message.channel.id]
+      }, 1000)
+    }
+
+    // First to X wins
+    if (game.pointsToWin && game.points[message.author.username] === game.pointsToWin) {
+      return setTimeout(() => {
+        message.channel.send(`${message.author.username} wins`)
+        delete game[message.channel.id]
+      }, 1000)
+    }
+
+    // Continue with next question
+    setTimeout(() => {
+      game.currentQuestion = game.questionPool.pop()
+      message.channel.send(`\`\`\`${game.currentQuestion.question}\`\`\``)
+    }, 1000)
+  })
+  .usage('!ans <your answer>')
+  .define('attempt to answer the trivia question')
+}
---
+++ plugins/trivia/question_sets/default.js
@@ -0,0 +1,17 @@
+module.exports = [
+  { question: `In what country would you find the Yellow River?`, answer: 'China' },
+  { question: `Formerly known as Bedloe's Island, what is the current name of the island where the Statue of Liberty is located?`, answer: 'Liberty Island' },
+  { question: `Sardinia, the second largest island in the Mediterranean Sea, is an autonomous region of what country?`, answer: 'Italy' },
+  { question: `Beirut is the capital and largest city of what country?`, answer: 'Lebanon' },
+  { question: `The Grand Canyon is located in which U.S. state?`, answer: 'Arizona' },
+  { question: `Located in Northwestern Turkey, which strait separates Europe and Asia?`, answer: 'Bosphorus Strait' },
+  { question: `South Africa completely surrounds which other African nation?`, answer: 'Lesotho' },
+  { question: `Europe is separated from Asia by which mountain range?`, answer: 'Ural Mountains' },
+  { question: `What is the largest country in North America?`, answer: 'Canada' },
+  { question: `Which of the great lakes does not share a border with Canada?`, answer: 'Lake Michigan' },
+  { question: `What is the national language of India?`, answer: 'Hindi' },
+  { question: `What is the largest island in the Caribbean Sea?`, answer: 'Cuba' },
+  { question: `Which city in the United States is known as the "Windy City"?`, answer: 'Chicago' },
+  { question: `What city is the capital of China?`, answer: 'Beijing' },
+  { question: `What city is the capital of Canada?`, answer: 'Ottawa' }
+]
